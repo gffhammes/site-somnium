1e83ff5c09ba97295915c972a9e2afe65ab4d26a
Guilherme Hammes <gffhammes@gmail.com>
2025-01-11 17:29:08 -0300

    feat: create hero section

---
+++ src/app/theme.ts
@@ -0,0 +1,32 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+import { Rajdhani } from "next/font/google";
+
+const rajdhani = Rajdhani({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: rajdhani.style.fontFamily,
+    fontWeightRegular: 500,
+    h1: {
+      fontSize: 16,
+      fontWeight: 500,
+    },
+    button: {
+      fontWeight: 700,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#0E4E58",
+    },
+    secondary: {
+      main: "#F6A131",
+    },
+  },
+});
+
+export default theme;
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,72 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import logo from "../../public/images/logo-somnium.png";
+import bgImage from "../../public/images/79b7c0b7b91ddd1b2e8ae52151915909.png";
+
+export interface IHeroSectionProps {}
+
+export const HeroSection = (props: IHeroSectionProps) => {
+  const logoAspectRatio = logo.width / logo.height;
+
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        height: "50vh",
+        backgroundImage: `url("${bgImage.src}")`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+
+        "&:before": {
+          content: '""',
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          top: 0,
+          right: 0,
+          backgroundColor: "primary.dark",
+          opacity: 0.8,
+          mixBlendMode: "multiply",
+        },
+      }}
+    >
+      <Container sx={{ height: "100%", position: "relative" }}>
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          sx={{ height: "100%" }}
+          gap={4}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              width: "100%",
+              maxWidth: "15rem",
+              aspectRatio: logoAspectRatio,
+            }}
+          >
+            <Image src={logo.src} alt="Somnium" fill />
+          </Box>
+
+          <Typography variant="h1" textAlign="center" color="white">
+            Chega de se preocupar com as tarefas financeiras, hora de focar no{" "}
+            <strong>crescimento do seu negócio!</strong>
+          </Typography>
+        </Stack>
+      </Container>
+
+      <Box
+        sx={{
+          position: "absolute",
+          top: "100%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Button variant="contained" color="secondary">
+          Clique aqui e saiba mais!
+        </Button>
+      </Box>
+    </Box>
+  );
+};

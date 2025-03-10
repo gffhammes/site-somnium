640076782218acbddd30d96fad8be26158595744
Guilherme Hammes <gffhammes@gmail.com>
2025-03-10 15:39:50 -0300

    feat: adjust first section

--- src/components/WhatWeDoSection/WhatWeDoSectionDesktop.tsx
+++ src/components/WhatWeDoSection/WhatWeDoSectionDesktop.tsx
@@ -12,7 +12,7 @@ export const WhatWeDoSectionDesktop = (props: IWhatWeDoSectionDesktopProps) => {
   const [openIndex, setOpenIndex] = useState(0);
 
   return (
-    <Container maxWidth="md">
+    <Container>
       <Stack gap={8}>
         <Typography variant="h2">O que fazemos:</Typography>
 
@@ -22,8 +22,8 @@ export const WhatWeDoSectionDesktop = (props: IWhatWeDoSectionDesktopProps) => {
           viewport={{ once: true, margin: "-25%" }}
           transition={{ duration: 1 }}
         >
-          <Stack direction="row">
-            <Stack sx={{ flex: "1 1 50%" }}>
+          <Stack direction="row" gap={4} alignItems="center">
+            <Stack sx={{ flex: "0 0 25rem" }}>
               {whatWeDoItems.map((item, index) => {
                 const isSelected = openIndex === index;
 
@@ -33,13 +33,15 @@ export const WhatWeDoSectionDesktop = (props: IWhatWeDoSectionDesktopProps) => {
                     onClick={() => setOpenIndex(index)}
                     sx={{
                       backgroundColor: isSelected
-                        ? "primary.main"
+                        ? "rgba(14, 78, 88, 0.177)"
                         : "transparent",
                       transition: ".3s ease all",
                       cursor: "pointer",
-                      color: isSelected ? "white" : "primary.main",
+                      color: isSelected ? "primary.main" : "primary.light",
                       px: 2,
                       py: 1,
+                      border: "1px solid",
+                      borderColor: isSelected ? "primary.main" : "transparent",
                     }}
                   >
                     <Typography fontWeight={isSelected ? "bold" : "500"}>
@@ -51,9 +53,16 @@ export const WhatWeDoSectionDesktop = (props: IWhatWeDoSectionDesktopProps) => {
             </Stack>
 
             <Stack
-              sx={{ backgroundColor: "primary.main", p: 3, color: "white" }}
+              sx={{
+                backgroundColor: "primary.main",
+                p: 3,
+                color: "white",
+                height: { md: "35rem", lg: "22rem" },
+              }}
             >
-              <Typography>{whatWeDoItems[openIndex].text}</Typography>
+              <Typography whiteSpace="pre-wrap">
+                {whatWeDoItems[openIndex].text}
+              </Typography>
             </Stack>
           </Stack>
         </Animate>
